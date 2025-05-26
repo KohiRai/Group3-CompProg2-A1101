34231c4fe63f131e31942bd06ed8a5b0b534dd99
KohiRai <lr.jkestacio@mmdc.mcl.edu.ph>
2025-05-26 21:39:45 +0800

    Add files via upload

---
+++ A1101Group3MotorPHGUI/src/EmployeeList.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class EmployeeList {
+    private String EmployeeID;
+    private String EmployeeName;
+    private String EmployeeBirthday;
+    private String EmployeeGender;
+
+    public EmployeeList(String EmployeeID, String EmployeeName, String EmployeeBirthday, String EmployeeGender) {
+        this.EmployeeID = EmployeeID;
+        this.EmployeeName = EmployeeName;
+        this.EmployeeBirthday = EmployeeBirthday;
+        this.EmployeeGender = EmployeeGender;
+    }
+    
+    
+    
+    
+    private static void show(){
+    List<String> list = new ArrayList<>();
+    
+//    Collections.addAll(list, EmployeeID, EmployeeName, EmployeeBirthday, EmployeeGender);
+    }
+    
+}
+
+    
+
