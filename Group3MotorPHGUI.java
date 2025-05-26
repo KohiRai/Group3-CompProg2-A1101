20fb6a6ac5826d48f022dde1d3f3c7ac461a2d34
Kristel301995 <lr.kaustria@mmdc.mcl.edu.ph>
2025-05-27 00:23:34 +0800

    Add files via upload

--- Group3MotorPHGUI.java
+++ Group3MotorPHGUI.java
@@ -18,22 +18,29 @@ public class Group3MotorPHGUI extends javax.swing.JFrame {
     private void initComponents() {
 
         jButton1 = new javax.swing.JButton();
+        jLabel11 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         UserIDComboBox = new javax.swing.JComboBox<>();
         SearchEmployee = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         EmployeeDetails = new javax.swing.JTextArea();
-        EnterBasicSalary = new javax.swing.JTextField();
-        jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
-        EnterDaysWorked = new javax.swing.JTextField();
-        EnterOvertimeHours = new javax.swing.JTextField();
+        SalaryCalculator = new javax.swing.JLabel();
+        OvertimeHours = new javax.swing.JLabel();
+        BasicPayAmount = new javax.swing.JTextField();
         CalculateSalary = new javax.swing.JButton();
+        OvertimeHours1 = new javax.swing.JTextField();
+        BasicPay = new javax.swing.JLabel();
+        jLabel18 = new javax.swing.JLabel();
+        WorkedDays = new javax.swing.JLabel();
+        WorkedDays1 = new javax.swing.JTextField();
 
         jButton1.setText("jButton1");
+
+        jLabel11.setText("jLabel11");
+
+        jTextField1.setText("jTextField1");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -61,30 +68,46 @@ public class Group3MotorPHGUI extends javax.swing.JFrame {
         jScrollPane1.setViewportView(EmployeeDetails);
         EmployeeDetails.getAccessibleContext().setAccessibleName("EmployeeDetails");
 
-        EnterBasicSalary.setText("0");
-        EnterBasicSalary.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                EnterBasicSalaryActionPerformed(evt);
-            }
-        });
+        SalaryCalculator.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        SalaryCalculator.setText("SALARY CALCULATOR");
+        SalaryCalculator.setPreferredSize(new java.awt.Dimension(200, 20));
 
-        jLabel3.setText("Enter Basic Salary:");
+        OvertimeHours.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        OvertimeHours.setText("Overtime Hours:");
 
-        jLabel4.setText("Salary Calculator");
-        jLabel4.setPreferredSize(new java.awt.Dimension(200, 20));
+        BasicPayAmount.setText("0");
 
-        jLabel5.setText("Enter Overtime Hours:");
-
-        jLabel6.setText("Enter Days Worked:");
-
-        EnterDaysWorked.setText("0");
-
-        EnterOvertimeHours.setText("0");
-
-        CalculateSalary.setText("Calculate");
+        CalculateSalary.setBackground(new java.awt.Color(204, 204, 204));
+        CalculateSalary.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        CalculateSalary.setText("CALCULATE");
         CalculateSalary.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 CalculateSalaryActionPerformed(evt);
+            }
+        });
+
+        OvertimeHours1.setText("0");
+        OvertimeHours1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                OvertimeHours1ActionPerformed(evt);
+            }
+        });
+
+        BasicPay.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        BasicPay.setText("Basic Salary:");
+        BasicPay.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusGained(java.awt.event.FocusEvent evt) {
+                BasicPayFocusGained(evt);
+            }
+        });
+
+        WorkedDays.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        WorkedDays.setText("Days Worked:");
+
+        WorkedDays1.setText("0");
+        WorkedDays1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                WorkedDays1ActionPerformed(evt);
             }
         });
 
@@ -103,61 +126,72 @@ public class Group3MotorPHGUI extends javax.swing.JFrame {
                         .addComponent(SearchEmployee))
                     .addComponent(jScrollPane1)
                     .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(34, 34, 34)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                                    .addComponent(jLabel6)
-                                    .addGap(25, 25, 25))
-                                .addGroup(layout.createSequentialGroup()
-                                    .addComponent(jLabel3)
-                                    .addGap(33, 33, 33)))
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                                .addComponent(EnterBasicSalary)
-                                .addComponent(EnterDaysWorked, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                        .addGroup(layout.createSequentialGroup()
-                            .addComponent(jLabel5)
-                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                            .addComponent(EnterOvertimeHours, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addComponent(CalculateSalary))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(50, 50, 50)
-                        .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(41, Short.MAX_VALUE))
+                        .addGap(86, 86, 86)
+                        .addComponent(CalculateSalary)
+                        .addGap(43, 43, 43)
+                        .addComponent(jLabel18))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGap(30, 30, 30)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addComponent(WorkedDays)
+                                        .addGap(28, 28, 28))
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addComponent(OvertimeHours)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED))
+                                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                                        .addComponent(BasicPay)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)))
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(OvertimeHours1)
+                                    .addComponent(BasicPayAmount)
+                                    .addComponent(WorkedDays1, javax.swing.GroupLayout.PREFERRED_SIZE, 104, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addComponent(SalaryCalculator, javax.swing.GroupLayout.PREFERRED_SIZE, 199, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(9, 9, 9)))))
+                .addGap(0, 49, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(14, 14, 14)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(15, 15, 15)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(SalaryCalculator, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jLabel1)
+                        .addComponent(UserIDComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(SearchEmployee))
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(WorkedDays1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(WorkedDays)))
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel1)
-                            .addComponent(UserIDComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(SearchEmployee))
                         .addGap(15, 15, 15)
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 219, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(OvertimeHours)
+                            .addComponent(OvertimeHours1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(BasicPayAmount, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(BasicPay, javax.swing.GroupLayout.PREFERRED_SIZE, 19, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel3)
-                            .addComponent(EnterBasicSalary, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(6, 6, 6)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel6)
-                            .addComponent(EnterDaysWorked, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel5)
-                            .addComponent(EnterOvertimeHours, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(12, 12, 12)
-                        .addComponent(CalculateSalary)))
-                .addContainerGap(37, Short.MAX_VALUE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(20, 20, 20)
+                                .addComponent(jLabel18))
+                            .addGroup(layout.createSequentialGroup()
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(CalculateSalary)))))
+                .addContainerGap(23, Short.MAX_VALUE))
         );
 
         pack();
@@ -177,35 +211,40 @@ public class Group3MotorPHGUI extends javax.swing.JFrame {
 //        if(EmployeeInformation)
     }//GEN-LAST:event_SearchEmployeeActionPerformed
 
-    private void EnterBasicSalaryActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_EnterBasicSalaryActionPerformed
-        
-    }//GEN-LAST:event_EnterBasicSalaryActionPerformed
-
     private void CalculateSalaryActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_CalculateSalaryActionPerformed
-        // TODO add your handling code here:
+        // Get values from earnings inputs:
+        var BS = EnterBasicSalary.getText();
+        int BasicSalary = Integer.parseInt(BS);
+        var DW = EnterDaysWorked.getText();
+        int DaysWorked = Integer.parseInt(DW);
+        var OH = BasicPayAmount.getText();
+        int OvertimeHours = Integer.parseInt(OH);
         
-// TODO add your handling code here:
-  try {
-    int BasicSalary = Integer.parseInt(EnterBasicSalary.getText());
-    int DaysWorked = Integer.parseInt(EnterDaysWorked.getText());
-    int OvertimeHours = Integer.parseInt(EnterOvertimeHours.getText());
-
-   
-    int benefits = 1000; 
-    int grossSalary = BasicSalary + (OvertimeHours * 100); 
-
-    // Show result dialog (Task for Allysa)
-    JOptionPane.showMessageDialog(this, "Summary\n"
-        + "Benefits: " + benefits + "\n"
-        + "Gross Salary: " + grossSalary);
-
-} catch (NumberFormatException e) {
-    JOptionPane.showMessageDialog(this, 
-        "Please enter NUMBERS ONLY in the salary, days worked, and overtime fields.", 
-        "INPUT ERROR!", 
-        JOptionPane.WARNING_MESSAGE);
-} 
+        //Insert here a showmessagedialog using these below (Task for Allysa)
+         JOptionPane.showMessageDialog(this, "Summary\n"
+                                        + "Benefits: sampleInt\n"
+                                        + "Gross Salary: SampleInt");
+         
+        //exception(If statements where if user entered text instead of Int(Numbers) (Task for Rodel)
+        //if statement then add the code below inside the {} after you created working if statement/s
+        JOptionPane.showMessageDialog(this, 
+                              "Please enter Numbers instead of Text", 
+                              "CALCULATION ERROR!", 
+                              JOptionPane.WARNING_MESSAGE);
+        
     }//GEN-LAST:event_CalculateSalaryActionPerformed
+
+    private void OvertimeHours1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_OvertimeHours1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_OvertimeHours1ActionPerformed
+
+    private void WorkedDays1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_WorkedDays1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_WorkedDays1ActionPerformed
+
+    private void BasicPayFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_BasicPayFocusGained
+        // TODO add your handling code here:
+    }//GEN-LAST:event_BasicPayFocusGained
 
     public void setUpGUI(){
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -244,21 +283,24 @@ public class Group3MotorPHGUI extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel BasicPay;
+    private javax.swing.JTextField BasicPayAmount;
     private javax.swing.JButton CalculateSalary;
     private javax.swing.JTextArea EmployeeDetails;
-    private javax.swing.JTextField EnterBasicSalary;
-    private javax.swing.JTextField EnterDaysWorked;
-    private javax.swing.JTextField EnterOvertimeHours;
+    private javax.swing.JLabel OvertimeHours;
+    private javax.swing.JTextField OvertimeHours1;
+    private javax.swing.JLabel SalaryCalculator;
     private javax.swing.JButton SearchEmployee;
     private javax.swing.JComboBox<String> UserIDComboBox;
+    private javax.swing.JLabel WorkedDays;
+    private javax.swing.JTextField WorkedDays1;
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel11;
+    private javax.swing.JLabel jLabel18;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextField jTextField1;
     // End of variables declaration//GEN-END:variables
     private ArrayList<EmployeeList> EmployeeList = new ArrayList<>();
 }
